4f798cee4e4c9b7ee6507fc2493f72cf1fb78af4
amenihsini <129338064+amenihsini@users.noreply.github.com>
2024-01-21 15:50:37 +0100

    Delete conftest.py

--- conftest.py
+++
@@ -1,6 +0,0 @@
-# conftest.py
-from app import engine
-
-def pytest_sessionfinish(session, exitstatus):
-    # This fixture will dispose of the engine, ensuring the SQLite database is closed after tests
-    engine.dispose()
