ed5314351e73aca7456288d53a817bac11e70d6c
amenihsini <129338064+amenihsini@users.noreply.github.com>
2024-01-22 22:12:16 +0100

    Update imports.py

--- imports.py
+++ imports.py
@@ -1,28 +1,14 @@
-from fastapi import FastAPI, Depends
-from fastapi.responses import JSONResponse
-from fastapi.middleware.cors import CORSMiddleware
-from sqlalchemy.orm import Session
 from datetime import datetime, date, timedelta
 from uuid import uuid4
-from pydantic import BaseModel
-from models import Product, Transaction
-from database import create_app
-from typing import List
-from sqlalchemy import create_engine
-import uvicorn
-from fastapi.responses import PlainTextResponse
-from fastapi import HTTPException, Path, Body
-from typing import Union, Dict, List
-from datetime import datetime
-from uuid import uuid4
-from sqlalchemy import Column, String, Integer, ForeignKey, DateTime
-from sqlalchemy.orm import declarative_base, relationship
-from models import Base, Product 
-from fastapi import FastAPI
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from datetime import datetime
-from sqlalchemy import Column, String, Float, Date, Integer, ForeignKey, DateTime
-from sqlalchemy.orm import declarative_base, relationship
+from typing import List, Union, Dict
+from sqlalchemy import create_engine, Column, String, Float, Date, Integer, ForeignKey, DateTime
+from sqlalchemy.orm import sessionmaker, declarative_base, relationship
+from fastapi import FastAPI, Depends, HTTPException, Path, Body
+from fastapi.responses import JSONResponse, PlainTextResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import OAuth2AuthorizationCodeBearer
 
+from models import Product, Transaction, Base 
+from database import create_app
+
+import uvicorn
